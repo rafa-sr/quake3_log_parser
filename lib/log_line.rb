3c9369e03f40605edb9a9e26ac21c3d3402fbf9e
rafa-sr <rafa.a.solorzano@gmail.com>
2021-12-22 10:11:32 +0100

    event and kill? test and method added, killer test added

--- lib/log_line.rb
+++ lib/log_line.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
+require 'byebug'
 class LogLine
-  attr_accessor :line, :event, :killer, :death_cause
+  attr_accessor :tokens, :line
 
-  def self.parse(line)
-    log_line = new(line)
-  end
+  WORDS_WITHOUT_NUMBER_REGEX = /\b[^\d\W]+\b/
 
   def initialize(line)
     @line = line
+    @tokens = line.scan(WORDS_WITHOUT_NUMBER_REGEX)
+  end
+
+  def event
+    @event ||= @tokens.first
+  end
+
+  def death_cause
+    @death_cause ||= @tokens.last
+  end
+
+  def kill?
+    event == 'Kill'
   end
 end
--- spec/log_line_spec.rb
+++ spec/log_line_spec.rb
@@ -3,11 +3,57 @@
 require 'spec_helper'
 
 describe LogLine do
-  describe '.parse' do
-    it 'return a hash with the parser line'
+  let(:line) { '13:55 Kill: 3 4 6: Oootsimo killed Dono da Bola by MOD_ROCKET' }
+
+  describe '#event' do
+    it 'return the event that trigger the log' do
+      log_line = described_class.new(line)
+
+      expect(log_line.event).to eq 'Kill'
+    end
+
+    it 'return nil when there is event' do
+      line = '  2:33 ------------------------------------------------------------'
+      log_line = described_class.new(line)
+
+      expect(log_line.event).to eq nil
+    end
   end
 
   describe '#kill?' do
-    it 'return true when the line have a kill'
+    it 'return true when the event of the log is a kill' do
+      log_line = described_class.new(line)
+
+      expect(log_line.kill?).to be true
+    end
+
+    it 'return false when the event of the log is not a event' do
+      line = ' 26  0:00 ------------------------------------------------------------'
+      log_line = described_class.new(line)
+
+      expect(log_line.kill?).to be false
+    end
+
+    it 'return false when the event of the log is not a kill' do
+      line = ' 20:34 ClientConnect: 2'
+      log_line = described_class.new(line)
+
+      expect(log_line.kill?).to be false
+    end
+  end
+
+  describe '#killer' do
+    it 'return name of the player before the killed word' do
+      log_line = described_class.new(line)
+
+      expect(log_line.killer).to eq 'Oootsimo'
+    end
+
+    it 'return name of the player with white spaces before the killed word' do
+      line = '  2:11 Kill: 2 4 6: Dono da Bola killed Zeh by MOD_ROCKET'
+      log_line = described_class.new(line)
+
+      expect(log_line.killer).to eq 'Dono da Bola'
+    end
   end
 end

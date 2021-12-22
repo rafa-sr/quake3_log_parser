9822a48f8fd019147a09104a57fc47dade4cf97e
rafa-sr <rafa.a.solorzano@gmail.com>
2021-12-22 13:15:44 +0100

    killer_player method created and test pass

--- lib/log_line.rb
+++ lib/log_line.rb
@@ -4,19 +4,32 @@ require 'byebug'
 class LogLine
   attr_accessor :tokens, :line
 
-  WORDS_WITHOUT_NUMBER_REGEX = /\b[^\d\W]+\b/
+  WORDS_REGEX = /\w+/
 
   def initialize(line)
     @line = line
-    @tokens = line.scan(WORDS_WITHOUT_NUMBER_REGEX)
+    @tokens = line.scan(WORDS_REGEX)
   end
 
   def event
-    @event ||= @tokens.first
+    @event ||= @tokens[2]
   end
 
   def death_cause
     @death_cause ||= @tokens.last
+  end
+
+  def killer_player
+    before_players_info = 6
+    players_info = @tokens.drop(before_players_info)
+    killer_name = ''
+    players_info.each_with_index do |token, index|
+      return killer_name if token == 'killed'
+
+      killer_name += token if index.zero?
+      killer_name = "#{killer_name} #{token}" if index.positive?
+    end
+    nil
   end
 
   def kill?
--- spec/log_line_spec.rb
+++ spec/log_line_spec.rb
@@ -42,18 +42,25 @@ describe LogLine do
     end
   end
 
-  describe '#killer' do
+  describe '#killer_player' do
     it 'return name of the player before the killed word' do
       log_line = described_class.new(line)
 
-      expect(log_line.killer).to eq 'Oootsimo'
+      expect(log_line.killer_player).to eq 'Oootsimo'
     end
 
     it 'return name of the player with white spaces before the killed word' do
       line = '  2:11 Kill: 2 4 6: Dono da Bola killed Zeh by MOD_ROCKET'
       log_line = described_class.new(line)
 
-      expect(log_line.killer).to eq 'Dono da Bola'
+      expect(log_line.killer_player).to eq 'Dono da Bola'
+    end
+
+    it 'return nil if is not a kill event' do
+      line = ' 25:34 Item: 2 weapon_rocketlauncher'
+      log_line = described_class.new(line)
+
+      expect(log_line.killer_player).to eq nil
     end
   end
 end

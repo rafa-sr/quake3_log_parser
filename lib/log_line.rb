3363e7fe345876a0d1475ee88e2a5f90e09b50f2
rafa-sr <rafa.a.solorzano@gmail.com>
2021-12-24 12:16:10 +0100

    remove some test that does not have sence if the line is not a kill event

--- lib/log_line.rb
+++ lib/log_line.rb
@@ -5,8 +5,10 @@ class LogLine
   attr_accessor :tokens, :line
 
   WORDS_REGEX = /\w+/
-  BEFORE_KILLER_PLAYER_INDEX = 6.freeze
-
+  BEFORE_KILLER_PLAYER_INDEX = 6
+  KILLED = 'killed'
+  KILL = 'Kill'
+  BY = 'by'
 
   def initialize(line)
     @line = line
@@ -22,25 +24,27 @@ class LogLine
   end
 
   def killer_player
-    players_tokens = @tokens.drop(BEFORE_KILLER_PLAYER_INDEX)
-    parse_player_name(players_tokens, 'killed')
+    before_killed_tokens = @tokens.drop(BEFORE_KILLER_PLAYER_INDEX)
+    parse_player_name(before_killed_tokens, KILLED)
   end
 
   def death_player
     after_killed_index = find_killed_word_index + 1
     after_killed_tokens = @tokens.drop(after_killed_index)
-    parse_player_name(after_killed_tokens, 'by')
+    parse_player_name(after_killed_tokens, BY)
   end
 
   def find_killed_word_index
-    @tokens.each_with_index { |token , index | return index if token == 'killed'   }
+    @tokens.each_with_index { |token, index| return index if token == KILLED }
   end
 
-  def kill?
-    event == 'Kill'
+  def valid_kill_line?
+    event == KILL
   end
 
-  private def parse_player_name(player_tokens, split_by)
+  private
+
+  def parse_player_name(player_tokens, split_by)
     player_name = ''
     player_tokens.each_with_index do |token, index|
       return player_name if token == split_by
--- spec/log_line_spec.rb
+++ spec/log_line_spec.rb
@@ -20,25 +20,25 @@ describe LogLine do
     end
   end
 
-  describe '#kill?' do
+  describe '#valid_kill_line?' do
     it 'return true when the event of the log is a kill' do
       log_line = described_class.new(line)
 
-      expect(log_line.kill?).to be true
+      expect(log_line.valid_kill_line?).to be true
     end
 
     it 'return false when the event of the log is not a event' do
-      line = ' 26  0:00 ------------------------------------------------------------'
+      line = '  0:00 ------------------------------------------------------------'
       log_line = described_class.new(line)
 
-      expect(log_line.kill?).to be false
+      expect(log_line.valid_kill_line?).to be false
     end
 
     it 'return false when the event of the log is not a kill' do
       line = ' 20:34 ClientConnect: 2'
       log_line = described_class.new(line)
 
-      expect(log_line.kill?).to be false
+      expect(log_line.valid_kill_line?).to be false
     end
   end
 
@@ -56,11 +56,11 @@ describe LogLine do
       expect(log_line.killer_player).to eq 'Dono da Bola'
     end
 
-    it 'return nil if is not a kill event' do
-      line = ' 25:34 Item: 2 weapon_rocketlauncher'
+    it 'return world when is killed by <world>' do
+      line = ' 21:42 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT'
       log_line = described_class.new(line)
 
-      expect(log_line.killer_player).to eq nil
+      expect(log_line.killer_player).to eq 'world'
     end
   end
 
@@ -77,12 +77,13 @@ describe LogLine do
 
       expect(log_line.death_player).to eq 'Zeh'
     end
+  end
 
-    it 'return nil if is not a kill event' do
-      line = ' 25:34 Item: 2 weapon_rocketlauncher'
+  describe '#death_cause' do
+    it 'return the last word (MOD_ROCKET) in the log line' do
       log_line = described_class.new(line)
 
-      expect(log_line.death_player).to eq nil
+      expect(log_line.death_cause).to eq 'MOD_ROCKET'
     end
   end
 end

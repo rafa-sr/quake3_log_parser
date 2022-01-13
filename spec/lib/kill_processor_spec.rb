eee677251aeabb7dcea98a670be1117d5f020093
Rafael Solorzano <rafa.a.solorzano@gmail.com>
2022-01-13 13:41:31 +0100

    GameProcessor is now called GameParser (#8)
    * change the name of game_processor to game_parser, test and methods for game_parser
    * add test and method for print the total kills

--- spec/lib/kill_processor_spec.rb
+++ spec/lib/kill_processor_spec.rb
@@ -9,15 +9,15 @@ describe KillProcessor do
       before do
         client_2_begun
         client_3_begun
-        game_processor.process(LogLine.new(kill2_line))
+        game_parser.process(LogLine.new(kill2_line))
       end
 
       it 'increase the total kills by one' do
-        expect(game_processor.total_kills).to eq 1
+        expect(game_parser.total_kills).to eq 1
       end
 
       it 'increase the kills of the player 2 by one' do
-        expect(game_processor.players.first.kills).to eq 1
+        expect(game_parser.players.first.kills).to eq 1
       end
     end
   end
@@ -25,34 +25,34 @@ describe KillProcessor do
   context 'when the <world> kill player 2' do
     before do
       client_2_begun
-      game_processor.process(LogLine.new(kill2_world_line))
+      game_parser.process(LogLine.new(kill2_world_line))
     end
 
     it 'increase the total kills by one' do
-      expect(game_processor.total_kills).to eq 1
+      expect(game_parser.total_kills).to eq 1
     end
 
     it 'decrease the kills of the player 2 by one' do
-      expect(game_processor.players.first.kills).to eq(-1)
+      expect(game_parser.players.first.kills).to eq(-1)
     end
 
     it 'not appear in the player list' do
-      game_processor.players.each { |player| expect(player.name).not_to eq KillLogLine::WORLD }
+      game_parser.players.each { |player| expect(player.name).not_to eq KillLogLine::WORLD }
     end
   end
 
   context 'when player kill itself' do
     before do
       client_3_begun
-      game_processor.process(LogLine.new(self3_kill_line))
+      game_parser.process(LogLine.new(self3_kill_line))
     end
 
     it 'increase the total kills by one' do
-      expect(game_processor.total_kills).to eq 1
+      expect(game_parser.total_kills).to eq 1
     end
 
     it 'death player losses -1 kill' do
-      expect(game_processor.players.first.kills).to eq(-1)
+      expect(game_parser.players.first.kills).to eq(-1)
     end
   end
 end

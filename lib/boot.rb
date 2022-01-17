bac626ff000ae3852f60a330f647dbdd59e08e5e
Rafael Solorzano <rafa.a.solorzano@gmail.com>
2022-01-17 21:28:00 +0100

    Merge: branch Docker (#14)
    * added docker file
    * process log file from dir /app/temp/qgames.log
    * remove empty lines

--- lib/boot.rb
+++ lib/boot.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-require 'rubygems'
-require 'fileutils'
-require 'bundler/setup'
-
-require 'json'
 require 'bundler'
 Bundler.require :default
+
+require 'rubygems'
+require 'fileutils'
+require 'json'
+require 'logger'
+
 require_all 'lib/log_line.rb'
 require_all 'lib'

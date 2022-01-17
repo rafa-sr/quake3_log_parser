043a0cc8e3b3242a395fd01573c553332e133a40
rafa-sr <rafa.a.solorzano@gmail.com>
2022-01-17 13:56:05 +0100

    print method and test adde in Quake_file_parser

--- lib/quake_log_file_parser.rb
+++ lib/quake_log_file_parser.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
 class QuakeLogFileParser
-  attr_reader :matches
+  attr_reader :matches, :active_match, :game_parser
 
-  def initialize(file_path)
-    @file_path = file_path
+  def initialize
     @game_parser = nil
     @matches = []
     @active_match = false
+    @ranking_score = []
   end
 
-  def parse
-    File.foreach(File.join(ROOT, @file_path)) do |line|
-      log_line = LogLine.new(line)
+  def parse(line)
+    log_line = LogLine.new(line)
 
-      finish_match if @active_match && log_line.init_game?
+    finish_match if @active_match && log_line.init_game?
 
-      finish_match if @active_match && log_line.end_line?
+    finish_match if @active_match && log_line.end_line?
 
-      start_match if log_line.init_game?
+    start_match if log_line.init_game?
 
-      @game_parser.process(log_line) if @active_match
-    end
+    @game_parser.process(log_line) if @active_match
   end
 
   def start_match
@@ -32,5 +30,14 @@ class QuakeLogFileParser
   def finish_match
     @matches << @game_parser.print
     @active_match = false
+  end
+
+  def print
+    @matches.each_with_index do |match, index|
+      game_n = "game_#{index + 1}"
+      game_to_print = { game_n => match }
+      json = JSON.pretty_generate(game_to_print)
+      puts json
+    end
   end
 end

9484bedcf106abdad937a5a736d152bc72a62b4d
rafa-sr <rafa.a.solorzano@gmail.com>
2022-01-19 17:52:00 +0100

    rever kill processor

--- lib/kill_processor.rb
+++ lib/kill_processor.rb
@@ -7,7 +7,7 @@ class KillProcessor
 
   def initialize(client_processor)
     @total_kills = 0
-    @kills_by_means = {}
+    @kills_scoreboard = {}
     @client_processor = client_processor
   end
 
@@ -15,14 +15,16 @@ class KillProcessor
     add_kill(KillLogLine.new(log_line.line)) if log_line.kill_line?
   end
 
-  def add_kill(kill_line)
+  def add_kill(log_line)
+    return unless log_line.kill_line?
+
     @total_kills += 1
-    if kill_line.killer_id == KillLogLine::WORLD_ID || kill_line.death_id == kill_line.killer_id
-      add_score(kill_line.death_id, -1)
+    if log_line.killer_id == KillLogLine::WORLD_ID || log_line.death_id == log_line.killer_id
+      add_score(log_line.death_id, -1)
       return
     end
 
-    add_score(kill_line.killer_id, 1)
+    add_score(log_line.killer_id, 1)
   end
 
   private

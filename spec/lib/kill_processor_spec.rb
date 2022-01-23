e6b627ba17471def1abb0a3c33c64ebb9d1bae1a
rafa-sr <rafa.a.solorzano@gmail.com>
2022-01-23 11:55:42 +0100

    remove noise from PR

--- spec/lib/kill_processor_spec.rb
+++ spec/lib/kill_processor_spec.rb
@@ -57,51 +57,51 @@ describe KillProcessor do
       end
 
       it 'increase the kills of the player by one' do
-        player_score = ClientHelper.client_kills(id:               kill_log_line.id,
+        player_score = ClientHelper.client_kills(id: kill_log_line.id,
                                                  client_processor: client_processor)
         expect(player_score).to eq 1
       end
     end
+  end
 
-    context 'when the <world> kill player 2' do
-      let(:kill_world_line) { KillLogLine.new(kill2_world_line) }
+  context 'when the <world> kill player 2' do
+    let(:kill_world_line) { KillLogLine.new(kill2_world_line) }
 
-      before do
-        kill_processor.process(kill_world_line)
-      end
-
-      it 'increase the total kills by one' do
-        expect(kill_processor.total_kills).to eq 1
-      end
-
-      it 'decrease the kills of the player 2 by one' do
-        player_score = ClientHelper.client_kills(id:               kill_world_line.death_id,
-                                                 client_processor: client_processor)
-
-        expect(player_score).to eq(-1)
-      end
-
-      it 'not appear in the connected client list' do
-        client_processor.connected_clients.each { |player| expect(player.name).not_to eq KillLogLine::WORLD }
-      end
+    before do
+      kill_processor.process(kill_world_line)
     end
 
-    context 'when player kill itself' do
-      let(:kill_self_line) { KillLogLine.new(self3_kill_line) }
+    it 'increase the total kills by one' do
+      expect(kill_processor.total_kills).to eq 1
+    end
 
-      before do
-        kill_processor.process(kill_self_line)
-      end
+    it 'decrease the kills of the player 2 by one' do
+      player_score = ClientHelper.client_kills(id: kill_world_line.death_id,
+                                               client_processor: client_processor)
 
-      it 'increase the total kills by one' do
-        expect(kill_processor.total_kills).to eq 1
-      end
+      expect(player_score).to eq(-1)
+    end
 
-      it 'player losses -1 kill' do
-        player_score = ClientHelper.client_kills(id:               kill_self_line.killer_id,
-                                                 client_processor: client_processor)
-        expect(player_score).to eq(-1)
-      end
+    it 'not appear in the connected client list' do
+      client_processor.connected_clients.each { |player| expect(player.name).not_to eq KillLogLine::WORLD }
+    end
+  end
+
+  context 'when player kill itself' do
+    let(:kill_self_line) { KillLogLine.new(self3_kill_line) }
+
+    before do
+      kill_processor.process(kill_self_line)
+    end
+
+    it 'increase the total kills by one' do
+      expect(kill_processor.total_kills).to eq 1
+    end
+
+    it 'player losses -1 kill' do
+      player_score = ClientHelper.client_kills(id: kill_self_line.killer_id,
+                                               client_processor: client_processor)
+      expect(player_score).to eq(-1)
     end
   end
 end
